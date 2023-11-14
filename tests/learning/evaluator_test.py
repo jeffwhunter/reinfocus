d95b8b12ad2840cc039fc9e166ba7d44d8b4723f
Jeff Hunter <jhunter57@gmail.com>
2023-11-13 21:56:41 -0800

    Whoops
    Civilized a comment.

--- tests/learning/evaluator_test.py
+++ tests/learning/evaluator_test.py
@@ -9,7 +9,7 @@ class EvaluatorTest(unittest.TestCase):
     """TestCases for reinfocus.learning.evaluator."""
 
     def test_iqm_interval(self):
-        """Tests that iqm_interval does something, at least."""
+        """Tests that iqm_interval returns the IQM and appropriate percentiles."""
         tu.arrays_close(self, eva.iqm_interval([1, 2, 3, 4, 5]), [3, 2, 4])
 
 if __name__ == '__main__':
